8fe6e4e1e0a4cc8d5d1b51b2ad05908185464082
Ahda Firly Barori <51354643+adhfry@users.noreply.github.com>
2023-12-20 12:53:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,74 @@
+let highestZ = 1;
+
 class Paper {
   holdingPaper = false;
-  touchStartX = 0;
-  touchStartY = 0;
-  touchMoveX = 0;
-  touchMoveY = 0;
+  mouseTouchX = 0;
+  mouseTouchY = 0;
+  mouseX = 0;
+  mouseY = 0;
+  prevMouseX = 0;
+  prevMouseY = 0;
+  velX = 0;
+  velY = 0;
   rotation = Math.random() * 30 - 15;
   currentPaperX = 0;
   currentPaperY = 0;
   rotating = false;
 
   init(paper) {
-    paper.addEventListener('pointermove', (e) => {
-      e.preventDefault();
+    document.addEventListener('mousemove', (e) => {
+      if(!this.rotating) {
+        this.mouseX = e.clientX;
+        this.mouseY = e.clientY;
+        
+        this.velX = this.mouseX - this.prevMouseX;
+        this.velY = this.mouseY - this.prevMouseY;
+      }
+        
+      const dirX = e.clientX - this.mouseTouchX;
+      const dirY = e.clientY - this.mouseTouchY;
+      const dirLength = Math.sqrt(dirX*dirX+dirY*dirY);
+      const dirNormalizedX = dirX / dirLength;
+      const dirNormalizedY = dirY / dirLength;
 
-      if (this.holdingPaper) {
-        this.touchMoveX = e.clientX;
-        this.touchMoveY = e.clientY;
+      const angle = Math.atan2(dirNormalizedY, dirNormalizedX);
+      let degrees = 180 * angle / Math.PI;
+      degrees = (360 + Math.round(degrees)) % 360;
+      if(this.rotating) {
+        this.rotation = degrees;
+      }
 
-        const deltaX = this.touchMoveX - this.touchStartX;
-        const deltaY = this.touchMoveY - this.touchStartY;
+      if(this.holdingPaper) {
+        if(!this.rotating) {
+          this.currentPaperX += this.velX;
+          this.currentPaperY += this.velY;
+        }
+        this.prevMouseX = this.mouseX;
+        this.prevMouseY = this.mouseY;
 
-        this.currentPaperX += deltaX;
-        this.currentPaperY += deltaY;
+        paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px) rotateZ(${this.rotation}deg)`;
+      }
+    })
 
-        const maxX = window.innerWidth - paper.clientWidth;
-        const maxY = window.innerHeight - paper.clientHeight;
-
-        this.currentPaperX = Math.max(0, Math.min(this.currentPaperX, maxX));
-        this.currentPaperY = Math.max(0, Math.min(this.currentPaperY, maxY));
-
-        this.touchStartX = this.touchMoveX;
-        this.touchStartY = this.touchMoveY;
-
-        // Gunakan requestAnimationFrame untuk animasi yang lebih mulus
-        requestAnimationFrame(() => {
-          paper.style.transform = `translate3d(${this.currentPaperX}px, ${this.currentPaperY}px, 0) rotateZ(${this.rotation}deg)`;
-        });
+    paper.addEventListener('mousedown', (e) => {
+      if(this.holdingPaper) return; 
+      this.holdingPaper = true;
+      
+      paper.style.zIndex = highestZ;
+      highestZ += 1;
+      
+      if(e.button === 0) {
+        this.mouseTouchX = this.mouseX;
+        this.mouseTouchY = this.mouseY;
+        this.prevMouseX = this.mouseX;
+        this.prevMouseY = this.mouseY;
+      }
+      if(e.button === 2) {
+        this.rotating = true;
       }
     });
-
-    paper.addEventListener('pointerdown', (e) => {
-      if (this.holdingPaper) return;
-      this.holdingPaper = true;
-
-      paper.style.zIndex = "999"; // Menaikkan zIndex agar lebih tinggi
-      this.touchStartX = e.clientX;
-      this.touchStartY = e.clientY;
-    });
-
-    paper.addEventListener('pointerup', () => {
+    window.addEventListener('mouseup', () => {
       this.holdingPaper = false;
-      this.rotating = false;
-    });
-
-    paper.addEventListener('gesturestart', (e) => {
-      e.preventDefault();
-      this.rotating = true;
-    });
-
-    paper.addEventListener('gestureend', () => {
       this.rotating = false;
     });
   }
