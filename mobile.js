cd89ac467280b6e413d1a23e72db41241ada1e1c
Ahda Firly Barori <51354643+adhfry@users.noreply.github.com>
2023-12-20 08:06:07 +0700

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -20,38 +20,33 @@ class Paper {
   init(paper) {
     paper.addEventListener('pointermove', (e) => {
       e.preventDefault();
-      if(!this.rotating) {
-        this.touchMoveX = e.touches[0].clientX;
-        this.touchMoveY = e.touches[0].clientY;
-        
+
+      if (!this.rotating) {
+        this.touchMoveX = e.clientX;  // Gunakan e.clientX untuk mengakses posisi mouse atau pointer
+        this.touchMoveY = e.clientY;
+
         this.velX = this.touchMoveX - this.prevTouchX;
         this.velY = this.touchMoveY - this.prevTouchY;
       }
-        
-      const dirX = e.touches[0].clientX - this.touchStartX;
-      const dirY = e.touches[0].clientY - this.touchStartY;
-      const dirLength = Math.sqrt(dirX*dirX+dirY*dirY);
-      const dirNormalizedX = dirX / dirLength;
-      const dirNormalizedY = dirY / dirLength;
 
-      const angle = Math.atan2(dirNormalizedY, dirNormalizedX);
-      let degrees = 180 * angle / Math.PI;
-      degrees = (360 + Math.round(degrees)) % 360;
-      if(this.rotating) {
-        this.rotation = degrees;
-      }
-
-      if(this.holdingPaper) {
-        if(!this.rotating) {
+      if (this.holdingPaper) {
+        if (!this.rotating) {
           this.currentPaperX += this.velX;
           this.currentPaperY += this.velY;
         }
         this.prevTouchX = this.touchMoveX;
         this.prevTouchY = this.touchMoveY;
 
+        // Atur batasan agar kertas tetap berada di dalam area tertentu
+        const maxX = window.innerWidth - paper.clientWidth;
+        const maxY = window.innerHeight - paper.clientHeight;
+
+        this.currentPaperX = Math.max(0, Math.min(this.currentPaperX, maxX));
+        this.currentPaperY = Math.max(0, Math.min(this.currentPaperY, maxY));
+
         paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px) rotateZ(${this.rotation}deg)`;
       }
-    })
+    });
 
     paper.addEventListener('pointerdown', (e) => {
       if(this.holdingPaper) return; 
